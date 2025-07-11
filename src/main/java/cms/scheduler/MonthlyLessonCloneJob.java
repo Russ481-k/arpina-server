063f0f9f65d19f87f12e170cd412e0504e18e242
sb.youn <sb.youn@handylab.co.kr>
2025-07-11 17:16:53 +0900

    Revert "Update cron expression for MonthlyLessonCloneJob to schedule lessons at 15:50 on the 11th of each month"
    This reverts commit 59afe1c0c58a8442864865db9cd9ad9652076c17.

--- src/main/java/cms/scheduler/MonthlyLessonCloneJob.java
+++ src/main/java/cms/scheduler/MonthlyLessonCloneJob.java
@@ -21,7 +21,7 @@ public class MonthlyLessonCloneJob {
 
         private final LessonRepository lessonRepository;
 
-        @Scheduled(cron = "0 50 15 11 * ?")
+        @Scheduled(cron = "0 0 0 20 * ?")
         @Transactional
         public void cloneMonthlyLessons() {
                 YearMonth currentMonth = YearMonth.now();
